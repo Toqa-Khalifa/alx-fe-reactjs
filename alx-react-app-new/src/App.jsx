a12296ff13daf53cca87df0d1b6b678b754467db
Toqa-Khalifa <toqa.khalifa99@gmail.com>
2025-11-09 23:58:41 +0200

    Refactored to use Context API

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,17 +1,16 @@
-// src/App.jsx
 import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
-import Counter from './components/Counter';
+import Counter from "./components/Counter"; 
 import UserContext from './components/UserContext';
 
 function App() {
-  const userData = { 
-    name: "Sara Ahmed", 
-    age: 26, 
-    bio: "Front-end enthusiast who loves React." 
+  const userData = {
+    name: "Sara Ahmed",
+    age: 26,
+    bio: "Front-end enthusiast who loves React."
   };
 
   return (
