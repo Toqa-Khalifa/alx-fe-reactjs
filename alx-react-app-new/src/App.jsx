de7954a98c34b4d3715710460c2c5768ed7b9951
Toqa-Khalifa <toqa.khalifa99@gmail.com>
2025-11-09 17:02:01 +0200

    add counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,12 +3,15 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
+import Counter from "./components/Counter"; 
+
 
 function App() {
   return (
     <div style={{ fontFamily: 'Arial, sans-serif', backgroundColor: '#f2f6fb', minHeight: '100vh', paddingBottom: '20px' }}>
       <Header />
       <MainContent />
+      <Counter />
       <UserProfile name="Sara Ahmed" age={26} bio="Front-end enthusiast who loves React." />
       <Footer />
     </div>
