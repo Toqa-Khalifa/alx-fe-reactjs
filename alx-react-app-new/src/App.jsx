f195789d86d1cf960b97e34287b76287ba926138
Toqa-Khalifa <toqa.khalifa99@gmail.com>
2025-11-10 00:03:24 +0200

    Fix: move UserContext to src root and update imports for Context API

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,8 +3,8 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
-import Counter from "./components/Counter"; 
-import UserContext from './components/UserContext';
+import Counter from "./components/Counter";
+import UserContext from './UserContext';
 
 function App() {
   const userData = {
