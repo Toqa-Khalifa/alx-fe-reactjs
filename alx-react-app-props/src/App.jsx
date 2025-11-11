168ba570da127a53ab91cef4b2b2ad89ac9d82ee
Toqa-Khalifa <toqa.khalifa99@gmail.com>
2025-11-11 20:23:28 +0200

    Fix: update structure and naming for Context API task

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,34 +1,16 @@
 import React from "react";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import UserProfile from "./components/UserProfile";
-import Footer from "./components/Footer";
-import Counter from "./components/Counter";
+import ProfilePage from "./ProfilePage";
 import UserContext from "./UserContext";
 
 function App() {
   const userData = {
     name: "Sara Ahmed",
-    age: 26,
-    bio: "Front-end enthusiast who loves React."
+    email: "sara.ahmed@example.com",
   };
 
   return (
     <UserContext.Provider value={userData}>
-      <div
-        style={{
-          fontFamily: "Arial, sans-serif",
-          backgroundColor: "#f2f6fb",
-          minHeight: "100vh",
-          paddingBottom: "20px",
-        }}
-      >
-        <Header />
-        <MainContent />
-        <Counter />
-        <UserProfile />
-        <Footer />
-      </div>
+      <ProfilePage />
     </UserContext.Provider>
   );
 }
