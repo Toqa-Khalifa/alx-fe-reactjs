7653f888b4c530e2b5cb835a4ce2d0b4e96dc3ed
toqa <toqa.khalifa99@gmail.com>
2025-12-03 16:45:48 +0200

    Initial React setup for GitHub User Search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <p>Search for GitHub users</p>
+    </div>
+  );
+}
+
+export default App;
+
